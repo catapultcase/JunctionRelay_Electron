7c6e46d0907d4182165299786e0b05e939643aa5
catapultcase <rustyone@gmail.com>
2025-08-16 11:21:45 -0500

    pi compile

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,14 @@
 // renderer/src/main.tsx (or index.tsx if that's your file name)
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-declare global {
-  interface Window {
-    ipcRenderer?: {
-      on: (channel: string, listener: (event: any, ...args: any[]) => void) => void;
-      off: (channel: string, listener: (...args: any[]) => void) => void;
-      send: (channel: string, ...args: any[]) => void;
-      invoke: (channel: string, ...args: any[]) => Promise<any>;
-    };
-  }
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+// Use the existing types from electron-env.d.ts
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // Safe demo listener (optional)
